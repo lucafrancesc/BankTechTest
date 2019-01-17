29810b10363dfc6a462aef769c8ffd49960eb223
Luca <eto.lucafrancesco@gmail.com>
2019-01-17 11:40:25 +0000

    Liner added

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -1,17 +1,17 @@
 describe('Statement', function(){
-  var statement;
-  var transactions;
+	var statement;
+	var transactions;
 
-  beforeEach(function() {
-   statement = new Statement();
-   transactions = jasmine.createSpy('transactions');
- });
+	beforeEach(function() {
+		statement = new Statement();
+		transactions = jasmine.createSpy('transactions');
+	});
 
-  describe('returnStatement', function() {
-   it('Prints the header if no transactions', function(){
-     expect(statement.returnStatement(transactions)).toEqual('Date || Credit || Debit || Balance\n');
-   });
-  });
+	describe('returnStatement', function() {
+		it('Prints the header if no transactions', function(){
+			expect(statement.returnStatement(transactions)).toEqual('Date || Credit || Debit || Balance\n');
+		});
+	});
 
 
 });
--- spec/TransactionsSpec.js
+++ spec/TransactionsSpec.js
@@ -1,22 +1,22 @@
 'use strict';
 
 describe('transactions', function(){
-  var transaction;
+	var transaction;
 
-  beforeEach(function() {
-   transaction = new Transactions();
-   jasmine.clock().install();
-   var baseTime = new Date(2013, 9, 23);
-   jasmine.clock().mockDate(baseTime);
- });
+	beforeEach(function() {
+		transaction = new Transactions();
+		jasmine.clock().install();
+		var baseTime = new Date(2013, 9, 23);
+		jasmine.clock().mockDate(baseTime);
+	});
 
-  afterEach(function() {
-    jasmine.clock().uninstall();
-  });
+	afterEach(function() {
+		jasmine.clock().uninstall();
+	});
 
-  describe('singleTransaction', function() {
-    it('pushes date, amount and balance into the transactions array', function(){
-      expect(transaction.singleTransaction(1000,1000)).toEqual([ [ '2013/10/23', '1000.00', '1000.00' ] ]);
-    });
-  });
+	describe('singleTransaction', function() {
+		it('pushes date, amount and balance into the transactions array', function(){
+			expect(transaction.singleTransaction(1000,1000)).toEqual([ [ '2013/10/23', '1000.00', '1000.00' ] ]);
+		});
+	});
 });
